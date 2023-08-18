379069d889c9ef9b3abc7024ae309ea93980b1e1
Zaras00 <133480426+Zaras00@users.noreply.github.com>
2023-08-18 12:34:21 +0530

    Update Q1 (1).scala

--- Q1 (1).scala
+++ Q1 (1).scala
@@ -1,17 +1,25 @@
-package P8
-import scala.io.StdIn.readDouble
+package Lab_8
 
-object Q1 extends App{
-  def interest(money: Double): Double = {
-    money match {
-      case money if money <= 20000 => money * 0.02;
-      case money if money <= 200000 => money * 0.04;
-      case money if money <= 2000000 => money * 0.035;
-      case _ => money * 0.065;
-    }
+import scala.io.StdIn.readFloat
+
+object Q1 extends App {
+  def round(y: Double): Double = BigDecimal(y).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble;
+
+  //lambda function
+  val interest: Double => Double = (amount: Double) => {
+    amount match {
+      case amount if amount <= 20000 => amount * 0.02;
+      case amount if amount <= 200000 => amount * 0.04;
+      case amount if amount <= 2000000 => amount * 0.035;
+      case amount if(amount >= 2000000) => amount * .065;
+      }
   }
 
-  print("Enter the money: ");
-  var money = readDouble();
-  println(interest(money));
+  printf("Enter the deposit amount: ")
+
+  val input = readFloat();
+  val output: Double = interest(input)
+
+  print("Interest is Rs. " + round(output));;
+
 }
