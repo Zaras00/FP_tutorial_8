74bc82d0c7b0b4040e187a0f747382373f638fe3
Zaras00 <133480426+Zaras00@users.noreply.github.com>
2023-08-18 12:35:55 +0530

    Update Q2 (1).scala

--- Q2 (1).scala
+++ Q2 (1).scala
@@ -1,12 +1,21 @@
-package P8
+package Lab_8
 
-object Q2 extends App{
-  def PatternMatching(x: Int): String = x match {
-    case x if x < 0 => "Negative";
-    case x if x == 0 => "Zero";
-    case x if x % 2 == 0 => "Even";
-    case _ => "Odd";
+import scala.io.StdIn.{readFloat, readInt}
+
+object Q2 extends App {
+
+  //lambda function
+  val patternMatch: Int => String = (x: Int) => {
+    x match {
+      case x if (x <= 0) => "Negative/Zero";
+      case x if (x % 2 == 0) => "Even Number";
+      case _ => "Odd Number";
+    }
   }
 
-  println(PatternMatching(5));
-}
+  printf("Enter the number: ");
+
+  val input = readInt();
+  val output: String = patternMatch(input);
+
+  println(output);;
